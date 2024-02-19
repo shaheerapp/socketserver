016b7c18be40b924fbef657901f8a3ba54beac9b
Muhammad Shaheer <98802033+shaheerapp@users.noreply.github.com>
2024-02-19 14:01:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,43 +6,43 @@ import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
-// const io = new Server(server);
+const io = new Server(server);
 const port = 9000;
 
 
 
-// io.on('connection', async (socket) => {
-//     const currentUserId = socket.handshake.auth.token;
-//     console.log('a user connected to this: ' + currentUserId);
+io.on('connection', async (socket) => {
+    const currentUserId = socket.handshake.auth.token;
+    console.log('a user connected to this: ' + currentUserId);
 
-//     const editUserUrl = 'https://pakmachinery.com.pk/api/edit-profile/' + currentUserId;
+    const editUserUrl = 'https://pakmachinery.com.pk/api/edit-profile/' + currentUserId;
 
-//     try {
-//         await axios.post(editUserUrl, {
-//             isOnline: true,
-//         });
-//         io.emit('userOnline', { user_id: currentUserId });
-//     } catch (error) {
-//         console.error('API request on connection failed:', error);
-//     }
+    try {
+        await axios.post(editUserUrl, {
+            isOnline: true,
+        });
+        io.emit('userOnline', { user_id: currentUserId });
+    } catch (error) {
+        console.error('API request on connection failed:', error);
+    }
 
-//     socket.on('chatMessage', msg => {
-//         io.emit('chatMessage', msg);
-//     });
+    socket.on('chatMessage', msg => {
+        io.emit('chatMessage', msg);
+    });
 
-//     socket.on('disconnect', async () => {
-//         console.log('user disconnected: ' + currentUserId);
-//         try {
-//             await axios.post(editUserUrl, {
-//                 isOnline: false,
-//             });
-//             io.emit('userOffline', { user_id: currentUserId });
-//         } catch (error) {
-//             console.error('API request on connection failed:', error);
-//         }
+    socket.on('disconnect', async () => {
+        console.log('user disconnected: ' + currentUserId);
+        try {
+            await axios.post(editUserUrl, {
+                isOnline: false,
+            });
+            io.emit('userOffline', { user_id: currentUserId });
+        } catch (error) {
+            console.error('API request on connection failed:', error);
+        }
 
-//     });
-// });
+    });
+});
 
 server.listen(port, () => {
     console.log('server running on port:' + port);
