ca09e83c960e58b8b59b62f44a1e961bdfef2130
Muhammad Shaheer <muhammadshaheer@Muhammads-MacBook-Pro.local>
2024-02-19 13:53:43 +0500

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        requireConfigFile: false, // Disable Babel config file checking
+    },
+};
